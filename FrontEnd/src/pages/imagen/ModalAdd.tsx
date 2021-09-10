12b80dd478d050c2730e42b91605e23bef5c60d0
matthiass2 <matias_69914081@hotmail.com>
2021-09-10 17:33:50 -0300

    extraction of predominant color from images

--- FrontEnd/src/pages/imagen/ModalAdd.tsx
+++ FrontEnd/src/pages/imagen/ModalAdd.tsx
@@ -3,6 +3,7 @@ import React, { ChangeEvent, useEffect, useState } from 'react'
 import { Link, useHistory  } from 'react-router-dom'
 import api from '../../services/api'
 import  * as utils from '../../utils/ultil'
+import Color, { Palette } from "color-thief-react";
 
 interface Modal{
     ModalVisibleProps: any;
@@ -20,15 +21,22 @@ interface Album{
     user_id: number
 }
 
-
+interface ImageData{
+    title: string;
+    description: string;
+    date: string;
+    size: number;
+    color: string;
+}
 
 export default function ModalAdd({ModalVisibleProps}: Modal) {
     const [ userData, setUserData ] = useState<User>(Object);
     const [album, setAlbum] = useState<Album>(Object)
-
-
-    const [image,setImage] = useState<File>(Object);
-
+    const [UrlImage ,setUrlImage] = useState<string>('')
+    const [colorPred, setColorPred] = useState<string>('')
+    const [date, setDate] = useState<string>('')
+    const [imageFile,setImageimageFile] = useState<File>(Object);
+    const [imageData,setImageData] = useState<ImageData>(Object);
 
     function handleImages(event: ChangeEvent<HTMLInputElement>){
 
@@ -37,12 +45,16 @@ export default function ModalAdd({ModalVisibleProps}: Modal) {
 
         const imageSelected = event.target.files[0]
         
-        console.log(event.target.files[0]);
-        
-        setImage(imageSelected);
+        setImageData({...imageData, date: utils.getDateTime(imageSelected)})
+
+        setImageimageFile(imageSelected);
       
+        setUrlImage(URL.createObjectURL(imageSelected))
+
     }
 
+    console.log(imageData);
+    
 
     useEffect(() => {
         setUserData(utils.getUserStorages())
@@ -59,11 +71,18 @@ export default function ModalAdd({ModalVisibleProps}: Modal) {
         }
     }
 
-    console.log(userData);
-    
     return (
         <div className="container-modal">
-
+            <Color src={UrlImage} crossOrigin="anonymous" format="hex">
+                {({ data, loading }) => {
+                
+                if(!data) return;
+                
+                setImageData({...imageData, color: data});
+                
+                return null;
+                }}
+            </Color>
             <div className="modal">
 
                 <button className="closeBtn" onClick={()=>{ModalVisibleProps(false)}}>Fechar</button>
@@ -75,25 +94,26 @@ export default function ModalAdd({ModalVisibleProps}: Modal) {
                     </div>
                     
                     <div className="input-Modal-file">
-                        <p>{image.name}</p>
+                        <p>{imageFile.name}</p>
                         <label htmlFor='selecao-arquivo' >Selecionar um arquivo </label>
                         <input type="file" id="selecao-arquivo" onChange={handleImages}/>
                     </div>
                     
                     <div className="input-modal-text">
-                        <input type="text" name="" id="" />
+                        <input type="text" placeholder="Titulo"/>
+                    </div>
+                    
+                    <div className="input-modal-text">
+                        <textarea id="description" rows={10} cols={30} placeholder="Descrição" onChange={(e)=>{setImageData({...imageData, description: e.target.value})}} required></textarea>
                     </div>
 
                     <div className="input-modal-text">
-                        <input type="text" name="" id="" />
+                        <input type="text" placeholder="Data/Hora de aquisição" value={imageData.date} required/>
                     </div>
-
+                
                     <div className="input-modal-text">
-                        <input type="text" name="" id="" />
+                        <input type="text" placeholder="Cor predominante" style={{color: imageData.color}} value={imageData.color} required/>
                     </div>
-
-
-
                 </div>
 
                 <button onClick={addAlbum} className="concludeBtn">Concluir</button>
--- FrontEnd/src/utils/ultil.ts
+++ FrontEnd/src/utils/ultil.ts
@@ -13,3 +13,20 @@ export function getUserStorages(): User {
     let objectUser = userParserToJson(localStorage.getItem('@album/usuario'))
     return objectUser
 }
+
+export function getDateTime(file: File) {
+
+    const lastModifiedDate = new Date(file.lastModified);
+
+    const y = lastModifiedDate.getFullYear()
+    const m = String(lastModifiedDate.getMonth() + 1).padStart(2, '0')
+    const d = String(lastModifiedDate.getDate()).padStart(2, '0')
+
+    const hh = String(lastModifiedDate.getHours()).padStart(2, '0')
+    const mi = String(lastModifiedDate.getMinutes()).padStart(2, '0')
+    const ss = String(lastModifiedDate.getSeconds()).padStart(2, '0')
+
+    // yyyy + '-' + mm + '-' + dd + 'T' + hh + ':' + mi + ':' + ss + '.000000';
+
+    return (`${y}-${m}-${d} ${hh}:${mi}:${ss}`)
+}
